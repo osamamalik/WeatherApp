302ba20272e23ea939e52fc6c31d0d3098f7656f
Osama Malik <maliko@my.yorku.ca>
2020-05-30 01:35:08 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const location = process.argv[2]
+if (!location) {
+    console.log("Please provide a location.")
+} else {
+    geocode(location, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
